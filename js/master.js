fc62427a76fcac57a50b19e1b0fd0c8bd84de27d
Abu-omar2023 <essamabdallah737@yahoo.com>
2024-02-26 23:06:36 +0200

    allah akbar

--- js/master.js
+++ js/master.js
@@ -2,12 +2,21 @@ $(document).ready(function(){
 
     $(function (){
         $(".cover").delay(700).fadeOut(2000)
-    })
+    });
+
+    // statrt saidbar
+
+ 
+
+
+
+
+    // statrt saidbar
         
   
 $(".begphoto .first").hover(function (){
     $(".begphoto  .first img").animate({
-        width: "110%"
+        width: "105%"
     
     },500)
 },function (){
@@ -18,7 +27,7 @@ $(".begphoto .first").hover(function (){
 })
 $(".begphoto .last").hover(function (){
     $(".begphoto  .last img").animate({
-        width: "110%"
+        width: "105%"
     
     },500)
 },function (){
@@ -26,15 +35,34 @@ $(".begphoto .last").hover(function (){
         width: "100%"
     
     },500)
+});
+
+$(".saidbar ul li").hover(function (){
+    $(this).animate({
+        "padding-right": "20px"
+    
+    },200)
+},function (){
+    $(this).animate({
+        "padding-right": "0"
+    
+    },200)
 })
  
-// $(".begphoto .first").lave(function (){
-//     $(".begphoto  .first img").animate({
-//         width: "100%"
-    
-//     },500)
-// })
+// start Criminals
+$(".formcr").click(function () {
+    $(".Criminals").fadeIn(500)
+});
+$(".submit").click(function () {
+    $(".Criminals").delay(500).fadeOut(500);
+    $(".cover").css({
+        display:"none"
+    })
+})
 
+
+
+// end Criminals
     $('.navbar ul li a').click(function(){
         
         $(this).addClass('links').parent().siblings().find('a').removeClass('links');
